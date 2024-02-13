da7249513d11d514951cfa85048121233e20112c
Marcus DelPonte <madelponte@pm.me>
2024-02-13 16:04:09 -0500

    updates

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-sudo apt install -y ansible
-ansible-galaxy install geerlingguy.docker
-ansible-playbook -b -u root -c local -i "127.0.0.1," $1
+#sudo apt install -y ansible
+#ansible-galaxy install geerlingguy.docker
+#ansible-playbook -b -u root -c local -i "127.0.0.1," $1
+ansible-playbook -b -u mdelponte --ask-pass --ask-become-pass -i "$1," $2.yml
