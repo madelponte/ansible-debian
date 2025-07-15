642ca117213be02e5084eed5cdcd147679f3e384
Marcus DelPonte <madelponte@pm.me>
2025-07-15 14:03:15 -0400

    update ssh config

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-#sudo apt install -y ansible
-#ansible-galaxy install geerlingguy.docker
+sudo apt install -y ansible
+ansible-galaxy install geerlingguy.docker
 #ansible-playbook -b -u root -c local -i "127.0.0.1," $1
 ansible-playbook -b -u mdelponte --ask-pass --ask-become-pass -i "$1," $2.yml
+
